443fa1f3a45c75175c0933f00fc01a642e80eee9
pagarevijayy <pagarevj97@gmail.com>
2021-02-16 20:55:21 +0530

    working on image layout and optimization

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -9,9 +9,42 @@ import { UtilsService } from 'src/app/services';
 })
 export class HomepageComponent implements OnInit {
 
-  sectionAHeading:string = 'Natural Beads Collection';
-  sectionBHeading:string = 'Pendants Collection';
-  sectionCHeading:string = 'Jewellery component';
+  sectionAHeading: string = 'Natural Beads Collection';
+  sectionBHeading: string = 'Pendants Collection';
+  sectionCHeading: string = 'Jewellery component';
+
+  transformationLayoutOne = [
+    { width: "100vw" }
+  ];
+
+  lqip = { active: true, quality: 1 };
+
+  showcaseImages = [
+    {
+      name: 'image identifier',
+      url: 'https://ik.imagekit.io/pagarevijayytech/ecom-demo-project/prod-6_N3T0c8dM9.jpg'
+    },
+    {
+      name: 'image identifier',
+      url: 'https://ik.imagekit.io/pagarevijayytech/ecom-demo-project/prod-5_Bmvc6No5S.jpg'
+    },
+    {
+      name: 'image identifier',
+      url: 'https://ik.imagekit.io/pagarevijayytech/ecom-demo-project/product-4_IMTJrTZIW.webp'
+    },
+    {
+      name: 'image identifier',
+      url: 'https://ik.imagekit.io/pagarevijayytech/ecom-demo-project/product-3_rOFYI3OS2.jpg'
+    },
+    {
+      name: 'image identifier',
+      url: 'https://ik.imagekit.io/pagarevijayytech/ecom-demo-project/product-1_tbE8TJhRa.webp'
+    },
+    {
+      name: 'image identifier',
+      url: 'https://ik.imagekit.io/pagarevijayytech/ecom-demo-project/product-2_kMPTyfcDi.jpg'
+    }
+  ];
 
   isHandset$: Observable<boolean> = this._utilService.isHandset$;
 
