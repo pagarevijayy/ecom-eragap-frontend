db1f151dfba0e0bc8520f5e2326ce7b068315160
pagarevijayy <pagarevj97@gmail.com>
2021-02-22 19:43:55 +0530

    added a product click

--- src/app/components/utils/product-card/product-card.component.ts
+++ src/app/components/utils/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  routeLocation(){
+    this._router.navigate([`/product/bead/xyz`]);
   }
 
 }
