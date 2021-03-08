ddc3eecfb6eac4b7ee3c89525344ab4fc18f31a9
pagarevijayy <pagarevj97@gmail.com>
2021-03-08 15:09:43 +0530

    Rxjs observers bug resolved. Solution: Unsubscrine the observable on ngDestroy

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { StateManagementService, UtilsService } from 'src/app/services';
 import { browserData } from 'src/assets/data/inbrowser-data'
 
@@ -9,7 +9,8 @@ import { browserData } from 'src/assets/data/inbrowser-data'
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
+  private rxSubscription: Subscription = new Subscription();
 
   isHandset$: Observable<boolean> = this._utilService.isHandset$;
 
@@ -33,7 +34,6 @@ export class ProductsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    console.log('products oninit');
 
     // get routing details [route and slug]
     this._route.params.subscribe((params) => {
@@ -41,37 +41,43 @@ export class ProductsComponent implements OnInit {
       this.activeSubcategorySlug = params['subcategorySlug'];
     });
 
-    // categoryInformation
-    this._stateManagementService.itemCategoryClickedBroadcast$.subscribe((categoryInformation) => {
-      // flush existing data in variables before assigning values
-      this.currentCategoryLabel = null;
-      this.currentSubcategoryInfo = [];
+    // add a new rxjs subscription. || reason: subcribe to categoryInformation
+    this.rxSubscription.add(
+      this._stateManagementService.itemCategoryClickedBroadcast$.subscribe((categoryInformation) => {
+        // flush existing data in variables before assigning values
+        this.currentCategoryLabel = null;
+        this.currentSubcategoryInfo = [];
 
-      console.log('categoryInformation', categoryInformation);
+        // console.log('categoryInformation', categoryInformation);
 
-      // assigning category label
-      this.currentCategoryLabel = categoryInformation?.categoryLabel;
+        // assigning category label
+        this.currentCategoryLabel = categoryInformation?.categoryLabel;
 
-      // console.log('currentCategoryLabel', this.currentCategoryLabel);
-
-
-      // creating currentSubcategoryInfo [to be used in UI]
-      categoryInformation?.product_subcategories?.forEach(element => {
-        
-        // create an temporary object and push onto currentSubcategoryInfo
-        let subcategoryLabel = element?.subcategoryLabel;
-        let subcategorySlug = element?.subcategorySlug;
-        let subcategoryWeightage = element?.weightage;
-
-        let subcategoryHolder = { subcategoryLabel, subcategorySlug, subcategoryWeightage };
-
-        this.currentSubcategoryInfo.push(subcategoryHolder)
-      });
-
-      // console.log('currentSubcategoryInfo', this.currentSubcategoryInfo);
-    });
+        // console.log('currentCategoryLabel', this.currentCategoryLabel);
 
 
+        // creating currentSubcategoryInfo [to be used in UI]
+        categoryInformation?.product_subcategories?.forEach(element => {
+
+          // create an temporary object and push onto currentSubcategoryInfo
+          let subcategoryLabel = element?.subcategoryLabel;
+          let subcategorySlug = element?.subcategorySlug;
+          let subcategoryWeightage = element?.weightage;
+
+          let subcategoryHolder = { subcategoryLabel, subcategorySlug, subcategoryWeightage };
+
+          this.currentSubcategoryInfo.push(subcategoryHolder)
+        });
+
+        // console.log('currentSubcategoryInfo', this.currentSubcategoryInfo);
+      })
+    );
+
+  }
+
+
+  ngOnDestroy(): void {
+    this.rxSubscription.unsubscribe();
   }
 
 }
